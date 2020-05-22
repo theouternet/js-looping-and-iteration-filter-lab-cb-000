f7aa37de0daf10542441b72b9ec81aa39b01c7f4
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-22 01:37:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file
+function findMatching(drivers, str) {
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase() == str.toLowerCase();
+  });
+}
+
+
+function fuzzyMatch(drivers, str) {
+  return drivers.filter(function(driver) {
+    return driver.slice(0,str.length) == str;
+  });
+}
+
+
+function matchName(drivers, str) {
+  return drivers.filter(function(driver) {
+    return driver.name.toLowerCase() === str.toLowerCase();
+  });
+} 
