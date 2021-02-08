a5aecad769e04f472e1032b1240a63c82a032aa5
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2021-02-08 00:39:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 function findMatching(drivers, str) {
-  return drivers.filter(function(driver) {
-    return driver.toLowerCase() == str.toLowerCase();
-  });
+  drivers.filter(e => "str")
 }
 
 
 function fuzzyMatch(drivers, str) {
-  return drivers.filter(function(driver) {
-    return driver.slice(0,str.length) == str;
-  });
+
 }
 
 
 function matchName(drivers, str) {
-  return drivers.filter(function(driver) {
-    return driver.name.toLowerCase() === str.toLowerCase();
-  });
+
 } 
